f7bbb216c6675438760699ff75208ccf12ddee32
nikita262001 <nikita262001@mail.ru>
2020-02-15 09:20:52 +0300

    TestWorkGitOnAccountsUser

---
+++ TestVolk/AccauntsUser/AccauntsUser/Account.cs
@@ -0,0 +1,44 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccountsUser
+{
+    class Account
+    {
+        [BsonId]
+        public string login = null;
+        public string password = null;
+        public List<Сharacter> Person { get => person; set => person = value; }
+        private List<Сharacter> person = new List<Сharacter>();
+
+
+        public Account()
+        {
+            Console.WriteLine("Введите логин");
+            string _login = "Tester" /*Console.ReadLine()*/;
+            Console.WriteLine("Введите пароль");
+            string _password = "qwert" /*Console.ReadLine()*/;
+
+            login = _login;
+            password = _password;
+        }
+
+        public void AddPers()
+        {
+            Console.WriteLine("Введите Рассу перса");
+            string race = "Human" /*Console.ReadLine()*/;
+            Console.WriteLine("Введите Name перса");
+            string name = "Three" /*Console.ReadLine()*/;
+            Сharacter a = new Сharacter(race, name);
+            a.AddBuffs();
+            a.AddBunkItem();
+            a.AddItem();
+            a.AddLeanedSkills();
+            a.AddQuests();
+            a.AddUsingSkills();
+            Person.Add(a);
+        }
+    }
+}
---
+++ TestVolk/AccauntsUser/AccauntsUser/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+
+namespace AccountsUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account();
+
+            account.AddPers();
+            account.AddPers();
+
+            string connectionString = "mongodb://localhost";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("TestUsers");
+            IMongoCollection<Account> collection = database.GetCollection<Account>("UsersGame");
+            collection.InsertOne(account);
+        }
+    }
+}
